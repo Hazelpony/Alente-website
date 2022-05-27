051cc4299e1992b0cfc15556f1f6a323cfe4397f
andrecosta8224 <andrecosta8224@gmail.com>
2022-05-27 18:52:05 -0300

    fixed error

--- Front/alente/src/Componentes/footer/index.jsx
+++ Front/alente/src/Componentes/footer/index.jsx
@@ -1,10 +1,10 @@
 import "../CSS/style01.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const Rodape = () => {
 	return(
 	    <>
-		    <Link to={"/sobre-nos"}>Sobre Nós<Link/>
+		    <Link to={"/sobre-nos"}>Sobre Nós</Link>
 		  </>
     )
 }
