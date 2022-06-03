6da18cf1d92c21efd7304268739c91944d6dccaa
andrecosta8224 <andrecosta8224@gmail.com>
2022-06-02 21:27:56 -0300

    update

--- Front/alente/src/App.js
+++ Front/alente/src/App.js
@@ -1,6 +1,6 @@
-
+import "./CSS/geralstyle.css"
 import Cabecalho from "./Componentes/header/index";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Carrossel from "./Componentes/main/index";
 import Rodape from "./Componentes/footer/index";
 import Perfil from "./Componentes/perfil/index";
@@ -8,7 +8,7 @@ import MenuOpcoes from "./Componentes/menu_opcoes/index";
 import ComoFunciona from "./Componentes/como_funciona/index";
 import MenuSaude from "./Componentes/menu_saude/index";
 import Doacoes from "./Componentes/doacoes/index"
-import Cadastro from "./Componentes/cadastro/index"
+import Login from "./Componentes/cadastro/index"
 import FaleConosco from "./Componentes/fale_conosco/index"
 import SobreNos from "./Componentes/sobre_nos/index"
 
@@ -16,19 +16,19 @@ function App() {
   return (
     <div>
       <Router>
-        <Cabecalho/>
+	<Cabecalho/>
       	<Routes>
       		<Route path="/" exact element={<Carrossel/>}/>
-      		<Route path="/cadastro" element={<Cadastro/>}/>
+      		<Route path="/cadastro" element={<Login/>}/>
       		<Route path="/perfil" element={<Perfil/>}/>
-      		<Route path="/menu-opcoes" element={<MenuOpcoes/>}/>
-      		<Route path="/como-funciona" element={<ComoFunciona/>}/>
-      		<Route path="/menu-saude" element={<MenuSaude/>}/>
-      		<Route path="/fale-conosco" element={<FaleConosco/>}/>
+      		<Route path="/menuopcoes" element={<MenuOpcoes/>}/>
+      		<Route path="/comofunciona" element={<ComoFunciona/>}/>
+      		<Route path="/menusaude" element={<MenuSaude/>}/>
+      		<Route path="/faleconosco" element={<FaleConosco/>}/>
       		<Route path="/doacoes" element={<Doacoes/>}/>
-      		<Route path="/sobre-nos" element={<SobreNos/>}/>
+      		<Route path="/sobrenos" element={<SobreNos/>}/>
       	</Routes>
-      	<Rodape/>
+	<Rodape/>
       </Router>
     </div>
   );
