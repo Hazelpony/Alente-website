77a4416d83eff1071b6c38bc70a70fbdcb2d5f5e
andrecosta8224 <andrecosta8224@gmail.com>
2022-06-18 14:11:10 -0300

    update

--- Front/alente/src/Componentes/sobre_nos/historia_de_inicio/index.jsx
+++ Front/alente/src/Componentes/sobre_nos/historia_de_inicio/index.jsx
@@ -2,7 +2,20 @@ const HistInicio = () => {
   //JavaScript aqui se precisar
   return(
     <>
-      html aqui
+	<main>
+	  <section>
+	    <article><img src="" alt=""/></article>
+	    <article>
+		<h1>Nossa história</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi lacus, porta vel malesuada vitae, consectetur sit amet nibh. Pellentesque ut tortor non diam placerat luctus a at.</p>
+		<p>Veja mais sobre nosso começo: <a href="http://globoplay.globo.com/v/8476809">http://globoplay.globo.com/v/8476809</a></p>
+	    </article>
+	    <article><img src="" alt=""/></article>
+	  <section>
+	  <section>
+	    <article><img src="" alt=""/></article>
+	  </section>
+	</main>
     </>
   )
 }
