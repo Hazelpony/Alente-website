eeec2808886d47655d43f49c9580418a85fbe29d
andrecosta8224 <andrecosta8224@gmail.com>
2022-07-07 12:17:50 -0300

    modal updated

--- Front/alente/src/Componentes/fale_conosco/modal.jsx
+++ Front/alente/src/Componentes/fale_conosco/modal.jsx
@@ -1,19 +1,21 @@
 import {Link} from "react-router-dom";
+import "./index.css";
 import FaleConosco from "./index";
+import confete from "../../Imagens/confete.png";
 import {Modal, Button} from "react-bootstrap";
 
 const ModalContato = (props) => {
   return(
 	<>
 	    <Modal show={props.show} onHide={props.handleClose}>
-  	      <Modal.Header closeButton>
-		<Modal.Title>Obrigado por nos contatar!</Modal.Title>
+  	      <Modal.Header>
+		<Modal.Title className="txtcenter"><img src={confete}/>Obrigado por nos contatar!</Modal.Title>
 	      </Modal.Header>
-	      <Modal.Body>
+	      <Modal.Body className="fundomodalbody txtcenter">
 		<p>Em breve retornaremos seu contato!</p>
 	      </Modal.Body>
 	      <Modal.Footer>
-		<Button variant="secondary" onClick={props.handleClose}>
+		<Button style={{backgroundColor: "#AD6E6F"}} variant="secondary" onClick={props.handleClose}>
 		  Fechar
 		</Button>
 	      </Modal.Footer>
